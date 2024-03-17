a746eda5db9e4b2d66beb12ac90be5accd43ad78
[Elton Irokromo] <[e_irokromo@hotmail.com]>
2024-03-17 10:59:58 +0100

    navbar aangepast, login klaar, js toegevoegd

--- static/script.js
+++ static/script.js
@@ -1,8 +1,22 @@
-var dropdown = document.getElementById("dropdown");
+// ---------------NAVIGATIE-----------------
+function sidemenu() {
+  let menu = document.getElementById("sidemenu");
+  menu.classList.toggle("sidemenu");
+} 
 
-function openmenu() {
-  dropdown.style.right = "0";
-}
-function closemenu() {
-  dropdown.style.right = "-15rem";
+
+// ---------------FORMS-----------------
+function showpassword(x) {
+  x.classList.toggle("fa-regular");
+  x.classList.toggle("fa-solid");
+  x.classList.toggle("fa-eye");
+  x.classList.toggle("fa-eye-slash");
+
+  let inputElement = document.getElementById("wachtwoord");
+
+  if (inputElement.type === "password") {
+      inputElement.type = "text";
+    } else {
+      inputElement.type = "password";
+    }
 }
