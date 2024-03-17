fdc3f8e173a10629d5d7a620d9016531499d85f4
[Elton Irokromo] <[e_irokromo@hotmail.com]>
2024-03-17 15:09:53 +0100

    registeren af

--- static/script.js
+++ static/script.js
@@ -5,7 +5,7 @@ function sidemenu() {
 } 
 
 
-// ---------------FORMS-----------------
+// ---------------FORMS PASSWORD-----------------
 function showpassword(x) {
   x.classList.toggle("fa-regular");
   x.classList.toggle("fa-solid");
@@ -20,3 +20,40 @@ function showpassword(x) {
       inputElement.type = "password";
     }
 }
+
+function showrepassword(x) {
+  x.classList.toggle("fa-regular");
+  x.classList.toggle("fa-solid");
+  x.classList.toggle("fa-eye");
+  x.classList.toggle("fa-eye-slash");
+
+  let inputElement = document.getElementById("repassword");
+
+  if (inputElement.type === "password") {
+      inputElement.type = "text";
+    } else {
+      inputElement.type = "password";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const passwordField = document.getElementById("wachtwoord");
+  const repasswordField = document.getElementById("repassword");
+  const errorSpan = document.getElementById("pass");
+  const form = document.querySelector(".registreerform");
+
+  repasswordField.addEventListener("input", function() {
+    if (passwordField.value !== repasswordField.value) {
+      errorSpan.textContent = "De wachtwoorden komen niet overeen";
+    } else {
+      errorSpan.textContent = "";
+    }
+  });
+
+  form.addEventListener("submit", function(event) {
+  if (passwordField.value !== repasswordField.value) {
+    errorSpan.textContent = "De wachtwoorden komen niet overeen";
+    event.preventDefault(); // Voorkom dat het formulier wordt ingediend
+  }
+});
+});
