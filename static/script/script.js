2271f234b40a42a4ac77bca4080baef5bc876820
Evi <69012052+Evi-2003@users.noreply.github.com>
2024-04-01 21:22:55 +0200

    Added sorting and filtering functionality

--- static/script/script.js
+++ static/script/script.js
@@ -1,48 +1,50 @@
 // ---------------NAVIGATIE-----------------
 function sidemenu() {
-  let menu = document.getElementById('sidemenu')
-  menu.classList.toggle('sidemenu')
+  let menu = document.getElementById("sidemenu");
+  menu.classList.toggle("sidemenu");
 }
 // --------------PROFIEL-----------
-document.addEventListener('DOMContentLoaded', function () {
-  let listItems = document.querySelectorAll('.profieldropdown ul li')
-  let textvakken = document.querySelectorAll('main > div[class^="textvak"]')
+document.addEventListener("DOMContentLoaded", function () {
+  let listItems = document.querySelectorAll(".profieldropdown ul li");
+  let textvakken = document.querySelectorAll('main > div[class^="textvak"]');
 
   listItems.forEach(function (item, index) {
-    item.addEventListener('click', function () {
+    item.addEventListener("click", function () {
       // Verplaats de 'profielactive'-klasse van het huidige actieve lijstitem naar het geselecteerde lijstitem
-      document.querySelector('.profielactive').classList.remove('profielactive')
-      this.classList.add('profielactive')
+      document
+        .querySelector(".profielactive")
+        .classList.remove("profielactive");
+      this.classList.add("profielactive");
 
       // Verberg alle tekstvakken
       textvakken.forEach(function (textvak) {
-        textvak.style.display = 'none'
-      })
+        textvak.style.display = "none";
+      });
       // Toon alleen het tekstvak met dezelfde index als het geselecteerde lijstitem
-      textvakken[index].style.display = 'block'
-    })
-  })
-})
+      textvakken[index].style.display = "block";
+    });
+  });
+});
 
 // ---------------FILTER-----------------
-document.getElementById('filterBtn').addEventListener('click', function () {
-  console.log('test')
-  var filterSection = document.querySelector('.filter')
+document.getElementById("filterBtn").addEventListener("click", function () {
+  console.log("test");
+  var filterSection = document.querySelector(".filter");
   // Controleer of de filtersectie verborgen is
-  if (filterSection.style.display === 'none') {
+  if (filterSection.style.display === "none") {
     // Als het verborgen is, maak het zichtbaar
-    filterSection.style.display = 'block'
+    filterSection.style.display = "block";
   } else {
     // Als het zichtbaar is, verberg het
-    filterSection.style.display = 'none'
+    filterSection.style.display = "none";
   }
-})
+});
 // -------------PROFIEL VERZOEKEN-------------
 async function laadVerzoeken(verzoeken) {
-  let list = document.getElementById('verzoekenList')
+  let list = document.getElementById("verzoekenList");
 
   verzoeken.forEach((verzoek) => {
-    let verzoekItem = document.createElement('li')
+    let verzoekItem = document.createElement("li");
     verzoekItem.innerHTML = ` <img src="img/bedreigde-sneeuwluipaard-die-in-de-aardhabitat-rust-wilde-dieren-in-gevangenschap-mooie-aziatische-katachtige-en-carnivoor-uncia-uncia kopie.jpg" alt="">
       <div>
         <h2>${verzoek.dierNaam}</h2>
@@ -53,148 +55,152 @@ async function laadVerzoeken(verzoeken) {
         <input type="hidden" name="verzoekId" value="${verzoek._id}">
         <button type="submit" name="accepteren" value="accepteren">✓</button>
         <button type="submit" name="accepteren" value="weigeren">𐤕</button>
-      </form> `
-    list.appendChild(verzoekItem)
-  })
+      </form> `;
+    list.appendChild(verzoekItem);
+  });
 }
-laadVerzoeken(verzoeken)
+laadVerzoeken(verzoeken);
 // ---------------FORMS PASSWORD-----------------
 function showpassword(x) {
-  x.classList.toggle('fa-regular')
-  x.classList.toggle('fa-solid')
-  x.classList.toggle('fa-eye')
-  x.classList.toggle('fa-eye-slash')
+  x.classList.toggle("fa-regular");
+  x.classList.toggle("fa-solid");
+  x.classList.toggle("fa-eye");
+  x.classList.toggle("fa-eye-slash");
 
-  let inputElement = document.getElementById('wachtwoord')
+  let inputElement = document.getElementById("wachtwoord");
 
-  if (inputElement.type === 'password') {
-    inputElement.type = 'text'
+  if (inputElement.type === "password") {
+    inputElement.type = "text";
   } else {
-    inputElement.type = 'password'
+    inputElement.type = "password";
   }
 }
 
 function showrepassword(x) {
-  x.classList.toggle('fa-regular')
-  x.classList.toggle('fa-solid')
-  x.classList.toggle('fa-eye')
-  x.classList.toggle('fa-eye-slash')
+  x.classList.toggle("fa-regular");
+  x.classList.toggle("fa-solid");
+  x.classList.toggle("fa-eye");
+  x.classList.toggle("fa-eye-slash");
 
-  let inputElement = document.getElementById('repassword')
+  let inputElement = document.getElementById("repassword");
 
-  if (inputElement.type === 'password') {
-    inputElement.type = 'text'
+  if (inputElement.type === "password") {
+    inputElement.type = "text";
   } else {
-    inputElement.type = 'password'
+    inputElement.type = "password";
   }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  let passwordField = document.getElementById('wachtwoord')
-  let repasswordField = document.getElementById('repassword')
-  let errorSpan = document.getElementById('pass')
-  let form = document.querySelector('.registreerform')
+document.addEventListener("DOMContentLoaded", function () {
+  let passwordField = document.getElementById("wachtwoord");
+  let repasswordField = document.getElementById("repassword");
+  let errorSpan = document.getElementById("pass");
+  let form = document.querySelector(".registreerform");
 
-  repasswordField.addEventListener('input', function () {
+  repasswordField.addEventListener("input", function () {
     if (passwordField.value !== repasswordField.value) {
-      errorSpan.textContent = 'De wachtwoorden komen niet overeen'
+      errorSpan.textContent = "De wachtwoorden komen niet overeen";
     } else {
-      errorSpan.textContent = ''
+      errorSpan.textContent = "";
     }
-  })
+  });
 
-  form.addEventListener('submit', function (event) {
+  form.addEventListener("submit", function (event) {
     if (passwordField.value !== repasswordField.value) {
-      errorSpan.textContent = 'De wachtwoorden komen niet overeen'
-      event.preventDefault() // Voorkom dat het formulier wordt ingediend
+      errorSpan.textContent = "De wachtwoorden komen niet overeen";
+      event.preventDefault(); // Voorkom dat het formulier wordt ingediend
     }
-  })
-})
+  });
+});
 
 // ---------------VRAGENLIJST CARD SLIDER-----------------
 
-let items = document.querySelectorAll('.slider .item')
-let active = 0
-let next = document.getElementById('next')
-let prev = document.getElementById('prev')
-let submitButton = document.querySelector('.vragenlijstbtn')
+let items = document.querySelectorAll(".slider .item");
+let active = 0;
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+let submitButton = document.querySelector(".vragenlijstbtn");
 
 function loadShow() {
-  items[active].style.transform = `none`
-  items[active].style.zIndex = 1
-  items[active].style.filter = 'none'
-  items[active].style.opacity = 1
+  items[active].style.transform = `none`;
+  items[active].style.zIndex = 1;
+  items[active].style.filter = "none";
+  items[active].style.opacity = 1;
   // show after
-  let stt = 0
+  let stt = 0;
   for (var i = active + 1; i < items.length; i++) {
-    stt++
-    items[i].style.transform = `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`
-    items[i].style.zIndex = -stt
-    items[i].style.filter = 'blur(5px)'
-    items[i].style.opacity = stt > 2 ? 0 : 0.6
+    stt++;
+    items[i].style.transform = `translateX(${120 * stt}px) scale(${
+      1 - 0.2 * stt
+    }) perspective(16px) rotateY(-1deg)`;
+    items[i].style.zIndex = -stt;
+    items[i].style.filter = "blur(5px)";
+    items[i].style.opacity = stt > 2 ? 0 : 0.6;
   }
-  stt = 0
+  stt = 0;
   for (var i = active - 1; i >= 0; i--) {
-    stt++
-    items[i].style.transform = `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`
-    items[i].style.zIndex = -stt
-    items[i].style.filter = 'blur(5px)'
-    items[i].style.opacity = stt > 2 ? 0 : 0.6
+    stt++;
+    items[i].style.transform = `translateX(${-120 * stt}px) scale(${
+      1 - 0.2 * stt
+    }) perspective(16px) rotateY(1deg)`;
+    items[i].style.zIndex = -stt;
+    items[i].style.filter = "blur(5px)";
+    items[i].style.opacity = stt > 2 ? 0 : 0.6;
   }
 }
 
-loadShow()
+loadShow();
 
 // Functie om de knoppen te tonen/verbergen op basis van de actieve vraag
 function toggleButtonsVisibility() {
   if (active === 0) {
-    prev.style.display = 'none' // Verberg de vorige knop als op de eerste vraag bent
+    prev.style.display = "none"; // Verberg de vorige knop als op de eerste vraag bent
   } else {
-    prev.style.display = 'block' // Toon de vorige knop als niet op de eerste vraag bent
+    prev.style.display = "block"; // Toon de vorige knop als niet op de eerste vraag bent
   }
 
   if (active === items.length - 1) {
-    next.style.display = 'none' // Verberg de volgende knop als op de laatste vraag bent
-    submitButton.style.display = 'block' // Toon de verzendknop als op de laatste vraag bent
+    next.style.display = "none"; // Verberg de volgende knop als op de laatste vraag bent
+    submitButton.style.display = "block"; // Toon de verzendknop als op de laatste vraag bent
   } else {
-    next.style.display = 'block' // Toon de volgende knop als niet op de laatste vraag bent
-    submitButton.style.display = 'none' // Verberg de verzendknop als niet op de laatste vraag bent
+    next.style.display = "block"; // Toon de volgende knop als niet op de laatste vraag bent
+    submitButton.style.display = "none"; // Verberg de verzendknop als niet op de laatste vraag bent
   }
 }
 
 // Controleer de zichtbaarheid van de knoppen bij het laden van de pagina
-toggleButtonsVisibility()
+toggleButtonsVisibility();
 
 // Voeg klikgebeurtenissen toe aan de knoppen om door de vragen te navigeren
 next.onclick = function () {
-  active = active + 1 < items.length ? active + 1 : active
-  loadShow()
-  toggleButtonsVisibility() // Controleer de zichtbaarheid van de knoppen na het veranderen van de actieve vraag
-}
+  active = active + 1 < items.length ? active + 1 : active;
+  loadShow();
+  toggleButtonsVisibility(); // Controleer de zichtbaarheid van de knoppen na het veranderen van de actieve vraag
+};
 
 prev.onclick = function () {
-  active = active - 1 >= 0 ? active - 1 : active
-  loadShow()
-  toggleButtonsVisibility() // Controleer de zichtbaarheid van de knoppen na het veranderen van de actieve vraag
-}
+  active = active - 1 >= 0 ? active - 1 : active;
+  loadShow();
+  toggleButtonsVisibility(); // Controleer de zichtbaarheid van de knoppen na het veranderen van de actieve vraag
+};
 
 // ---------------SLIDER INDEX-----------------
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   // And if we need scrollbar
   scrollbar: {
-    el: '.swiper-scrollbar',
+    el: ".swiper-scrollbar",
   },
-})
+});
