bb37b5931dea5ab5f61a4c43eb5a0d712e5f100f
[Elton Irokromo] <[e_irokromo@hotmail.com]>
2024-04-05 12:34:32 +0200

    javascript slider

--- static/script/script.js
+++ static/script/script.js
@@ -152,81 +152,80 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-// ---------------VRAGENLIJST CARD SLIDER-----------------
-
-let items = document.querySelectorAll(".slider .item");
-let active = 0;
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-let submitButton = document.querySelector('.vragenlijstbtn');
-console.log(items)
-if(items > 0) {
-  function loadShow(){
-    items[active].style.transform = `none`;
-    items[active].style.zIndex = 1;
-    items[active].style.filter = 'none';
-    items[active].style.opacity = 1;
-    // show after
-    let stt = 0;
-    for(var i = active + 1; i < items.length; i ++){
-        stt++;
-        items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
-        items[i].style.zIndex = -stt;
-        items[i].style.filter = 'blur(5px)';
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
-    }
-    stt = 0;
-    for(var i = (active - 1); i >= 0; i --){
-        stt++;
-        items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
-        items[i].style.zIndex = -stt;
-        items[i].style.filter = 'blur(5px)';
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
-    }
-}
-
-loadShow();
-}
-
-
-// Functie om de knoppen te tonen/verbergen op basis van de actieve vraag
-function toggleButtonsVisibility() {
-  if (active === 0) {
-    prev.style.display = "none"; // Verberg de vorige knop als op de eerste vraag bent
-  } else {
-    prev.style.display = "block"; // Toon de vorige knop als niet op de eerste vraag bent
-  }
-
-  if (active === items.length - 1) {
-    next.style.display = "none"; // Verberg de volgende knop als op de laatste vraag bent
-    submitButton.style.display = "block"; // Toon de verzendknop als op de laatste vraag bent
-  } else {
-    next.style.display = "block"; // Toon de volgende knop als niet op de laatste vraag bent
-    submitButton.style.display = "none"; // Verberg de verzendknop als niet op de laatste vraag bent
-  }
-}
-
-// Controleer de zichtbaarheid van de knoppen bij het laden van de pagina
-if (prev && next && submitButton) {
-toggleButtonsVisibility();
-}
-
-// Voeg klikgebeurtenissen toe aan de knoppen om door de vragen te navigeren
-if(next) {
-  next.onclick = function(){
-    active = active + 1 < items.length ?  active + 1 : active;
-    loadShow();
-    toggleButtonsVisibility(); // Controleer de zichtbaarheid van de knoppen na het veranderen van de actieve vraag
-}
-}
-
-if(prev)
-{prev.onclick = function(){
-  active = active - 1 >= 0 ? active -1 : active;
-  loadShow();
-  toggleButtonsVisibility(); // Controleer de zichtbaarheid van de knoppen na het veranderen van de actieve vraag
-}}
-
+              // ---------------VRAGENLIJST CARD SLIDER-----------------
+              document.addEventListener("DOMContentLoaded", function() {
+                let items = document.querySelectorAll(".slider .item");
+                let active = 0;
+                let next = document.getElementById('next');
+                let prev = document.getElementById('prev');
+                let submitButton = document.querySelector('.vragenlijstbtn');
+              
+                // Functie om de slider-items te laten zien op basis van de actieve index
+                function loadShow() {
+                  items[active].style.transform = `none`;
+                  items[active].style.zIndex = 1;
+                  items[active].style.filter = 'none';
+                  items[active].style.opacity = 1;
+              
+                  let stt = 0;
+                  for (var i = active + 1; i < items.length; i++) {
+                    stt++;
+                    items[i].style.transform = `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`;
+                    items[i].style.zIndex = -stt;
+                    items[i].style.filter = 'blur(5px)';
+                    items[i].style.opacity = stt > 2 ? 0 : 0.6;
+                  }
+              
+                  stt = 0;
+                  for (var i = (active - 1); i >= 0; i--) {
+                    stt++;
+                    items[i].style.transform = `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`;
+                    items[i].style.zIndex = -stt;
+                    items[i].style.filter = 'blur(5px)';
+                    items[i].style.opacity = stt > 2 ? 0 : 0.6;
+                  }
+                }
+              
+                // Functie om de zichtbaarheid van knoppen te controleren op basis van de actieve index
+                function toggleButtonsVisibility() {
+                  if (active === 0) {
+                    prev.style.display = "none"; // Verberg de vorige knop als op de eerste vraag bent
+                  } else {
+                    prev.style.display = "block"; // Toon de vorige knop als niet op de eerste vraag bent
+                  }
+              
+                  if (active === items.length - 1) {
+                    next.style.display = "none"; // Verberg de volgende knop als op de laatste vraag bent
+                    submitButton.style.display = "block"; // Toon de verzendknop als op de laatste vraag bent
+                  } else {
+                    next.style.display = "block"; // Toon de volgende knop als niet op de laatste vraag bent
+                    submitButton.style.display = "none"; // Verberg de verzendknop als niet op de laatste vraag bent
+                  }
+                }
+              
+                // Controleer de zichtbaarheid van knoppen bij het laden van de pagina
+                if (prev && next && submitButton) {
+                  toggleButtonsVisibility();
+                }
+              
+                // Voeg klikgebeurtenissen toe aan de knoppen om door de vragen te navigeren
+                if (next) {
+                  next.onclick = function() {
+                    active = active + 1 < items.length ? active + 1 : active;
+                    loadShow();
+                    toggleButtonsVisibility(); // Controleer de zichtbaarheid van knoppen na het veranderen van de actieve index
+                  }
+                }
+              
+                if (prev) {
+                  prev.onclick = function() {
+                    active = active - 1 >= 0 ? active - 1 : active;
+                    loadShow();
+                    toggleButtonsVisibility(); // Controleer de zichtbaarheid van knoppen na het veranderen van de actieve index
+                  }
+                }
+              });
+              
 
 // ---------------SLIDER-----------------
 // Controleer of het element met de klasse "swiper" aanwezig is op de pagina
